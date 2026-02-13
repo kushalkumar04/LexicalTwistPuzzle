a96fbb2f68685abbea5311a46ad45ae50aba4b2a
Kushal Kumar <gaddamkushal321@gmail.com>
2026-02-13 16:21:06 +0530

    [kushal] UC7: Implement rule-based output using vowel-consonant distribution

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,4 +1,6 @@
+import java.util.LinkedHashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class LexicalTwistPuzzle {
 
@@ -37,11 +39,13 @@ public class LexicalTwistPuzzle {
 
         } else {
 
-            // Combine words
             String combined = (word1 + word2).toUpperCase();
 
             int vowelCount = 0;
             int consonantCount = 0;
+
+            Set<Character> uniqueVowels = new LinkedHashSet<>();
+            Set<Character> uniqueConsonants = new LinkedHashSet<>();
 
             for (int i = 0; i < combined.length(); i++) {
 
@@ -51,14 +55,35 @@ public class LexicalTwistPuzzle {
 
                     if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
                         vowelCount++;
+                        uniqueVowels.add(ch);
                     } else {
                         consonantCount++;
+                        uniqueConsonants.add(ch);
                     }
                 }
             }
 
-            System.out.println("Vowels: " + vowelCount);
-            System.out.println("Consonants: " + consonantCount);
+            if (vowelCount > consonantCount) {
+
+                int count = 0;
+                for (char v : uniqueVowels) {
+                    System.out.print(v);
+                    count++;
+                    if (count == 2) break;
+                }
+
+            } else if (consonantCount > vowelCount) {
+
+                int count = 0;
+                for (char c : uniqueConsonants) {
+                    System.out.print(c);
+                    count++;
+                    if (count == 2) break;
+                }
+
+            } else {
+                System.out.println("Vowels and consonants are equal");
+            }
         }
     }
 }
